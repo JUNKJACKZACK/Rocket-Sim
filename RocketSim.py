ca3562d1580d03aee6ae4431a2823a56f01d318f
Zachary Donnelly <zacharydonnelly_IT@protonmail.com>
2022-09-28 20:33:41 -0600

    WORKING ON ERRORS

--- RocketSim.py
+++ RocketSim.py
@@ -12,17 +12,22 @@ FPS = 60
 ROCKET_IMAGE = pygame.image.load(os.path.join('Assets', 'rocket_shuttle.png'))
 ROCKET_TEST = pygame.transform.scale(ROCKET_IMAGE, (150, 150))
 
-ROCKET_START_POS = 400, 1050
+ROCKET_START_POS = 400, 1500
 
 def draw_window():
     WIN.fill(WHITE)
-    WIN.blit(ROCKET_TEST, (ROCKET_START_POS))
+    WIN.blit(ROCKET_TEST, (draw_rocket.x, draw_rocket.y))
     pygame.display.update()
 
 
+
 def main(): 
+    
+    draw_rocket = pygame.Rect(400, 1500, 150, 150)
+    
     clock = pygame.time.Clock()
     run = True
+    
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
